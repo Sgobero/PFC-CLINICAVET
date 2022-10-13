093d3108cd0cb2686083d76b5e61ba972ff94bab
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-13 20:28:56 -0300

    Adicionando duas funções
    Função verifyAccount 
     - verifica se ja existe os dados passados pelo cliente no banco de dados (email, cpf e rg)
    Função createAccount
     - se não houver os dados já registrados no banco de dados, os novos dados são inseridos (tabela usuarios e endereco)

--- backend/nossa versão/app/sts/Models/StsCadastro.php
+++ backend/nossa versão/app/sts/Models/StsCadastro.php
@@ -4,11 +4,57 @@ namespace Sts\Models;
 
 class StsCadastro{
 
-    public function index(){
+    /** function verifyAcount
+     * verifica se os dados CPF, RG ou EMAIL que o cliente tentou
+     * registrar já estão cadastradas no banco de dados (conta já criada)
+     * retorna false se já existe uma conta criada com esses dados e true se não 
+     */
+    public function verifyAccount(array $data): bool
+    {   
 
+        extract($data);
+
+        $stsSelect = new \Sts\Models\helpers\Stsselect();
+
+        $stsSelect->fullRead("SELECT email FROM usuario 
+                            WHERE idusuario = :cpf or rg = :rg or email = :email",
+                            "cpf={$cpf}&rg={$rg}&email={$email}");
+
+        $resultado =  $stsSelect->getResult();
+
+        if(empty($resultado)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-}
+    /**
+     * Responsavel por criar a conta do Usuário
+     */
+    public function createAccount(array $data): string|null
+    {
 
+        $stsCreate = new \Sts\Models\helpers\StsCreate();
+        $stsCreate->exeCreatre("endereco",$data[1]); //primeiro cria o endereco
+        $idEndereco = $stsCreate->getResult();
+
+        if(!empty($idEndereco)){
+
+            $data[0]['endereco'] = $idEndereco; // add endereco e id para a FK
+            
+            echo "<pre>"; var_dump($data[0]); echo "</pre>";
+            $stsCreate->exeCreatre("usuario",$data[0]);
+            $idCliente = $stsCreate->getResult();
+            //echo "id cliente: " . $idCliente . "<br>;
+            //echo "id endereco: " . $idEndereco . "<br> id:";
+            //var_dump($idCliente);
+            return $idCliente;
+
+        }else{
+            return null;
+        }
+    }
+}
 
 ?>
