b9be36ad5472652e98320cd3d0b8664d01ad8def
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-13 20:24:51 -0300

    Controller cadastro
    Adicionado novas chamadas de funções (da model StsCadastro)
    -veryfyAccount
    -createAccount

--- backend/nossa versão/app/sts/Controllers/Cadastro.php
+++ backend/nossa versão/app/sts/Controllers/Cadastro.php
@@ -7,31 +7,38 @@ class Cadastro{
     private array|string|null $date = [];
     private array|string|null $dataForm;
 
-    public function index(){
-            
+    public function index()
+    {    
         $this->dataForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
         if(!empty($this->dataForm['AddContMsg'])){
 
             unset($this->dataForm['AddContMsg']);
 
-            echo "<pre>"; var_dump($this->dataForm); echo "</pre>";
+            $stsCadastro = new \Sts\Models\StsCadastro();
 
-            $StsCadastro = new \Sts\Models\StsCadastro();
+            $this->data = array_chunk($this->dataForm, 7, true);
+
+            $resultVerify = $stsCadastro->verifyAccount($this->data[0]);
+
+            if($resultVerify == true){
+                $idUsuario = $stsCadastro->createAccount($this->data);
+                if(!empty($idUsuario)){
+                    echo "Usuario cadastrado com sucesso <br>";
+                    echo "Id do Usuario: " . $idUsuario;
+                }
+            }else{
+                echo "Usuario já cadastrado, tente com outros dados";
+            }
             
         }
 
-
         $stsCadastro = new \Sts\Models\StsCadastro();
-
 
         $loadView = new \Core\LoadView("sts/Views/cadastro", $this->date);
         
         $loadView->loadView();
-
     }
-
 }
-
 
 ?>
