663ef3fb1cae8e05b58dcb170565e9e7856cdec9
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-15 00:22:46 -0300

    Controller login funcional
    Adicionei o método login (do StsLogin) que verifica se o email e senha que o usuário passou esta no banco de dados, se sim por enquanto ele só retorna o id e mostra na tela.

--- backend/nossa versão/app/sts/Controllers/Login.php
+++ backend/nossa versão/app/sts/Controllers/Login.php
@@ -4,10 +4,32 @@ namespace Sts\Controllers;
 
 class Login{
     
-    
+    private array|string|null $date = [];
+    private array|string|null $dataForm;
 
     public function index(){
-        echo "Controller login";
+        
+        $this->dataForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+        if(!empty($this->dataForm['AddContMsg'])){
+
+            unset($this->dataForm['AddContMsg']);
+            
+            $stsLogin = new \Sts\Models\StsLogin();
+            $result = $stsLogin->login($this->dataForm);
+            if(!empty($result)){
+                extract($result[0]);
+                echo "Id do usuario: {$idusuario}";
+            }else{
+                echo "Usuario não existente";
+            }
+
+
+        }
+
+        $loadView = new \Core\LoadView('sts/Views/login', null);
+        $loadView->loadView();
+        
     }
     
 }
