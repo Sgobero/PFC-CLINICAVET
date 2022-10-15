e1a924b54e9ba929cbe38cf6b77e161ca14ed69b
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-15 17:40:59 -0300

    tela de login
    tela de login com método post e action vazia

---
+++ backend/nossa versão/app/sts/Views/login.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="">
+        
+        <h2> Login </h2>
+
+        <label>EMAIL: </label>
+        <input name="email" type="text" placeholder="email"> <br> <br>
+        
+        <label>SENHA: </label>
+        <input name="senha_usuario" type="text" placeholder="senha"> <br> <br>
+
+        <input name="AddContMsg" type="submit" value="Enviar" >
+
+    </form>
+
+</body>
+</html>
