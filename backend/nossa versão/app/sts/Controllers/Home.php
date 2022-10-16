3321dacba1fe3e9abc88b7a44be81c1b9f9ada70
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-16 14:02:31 -0300

    Controller home
    A tela home não precisa de nenhuma informação do banco de dados então não fiz ainda a model dela. 
    Aqui eu coloquei um sistema para destruir a session (temporário)

--- backend/nossa versão/app/sts/Controllers/Home.php
+++ backend/nossa versão/app/sts/Controllers/Home.php
@@ -4,8 +4,21 @@ namespace Sts\Controllers;
 
 class Home{
 
-    public function index(){
-        echo "Controller Home";
-    }
+    public function index()
+    {   
+        $dataForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
+        if(!empty($dataForm['session_destroy']))
+        {
+            unset($dataForm['session_destroy']);
+            
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            
+            session_destroy();
+        }
+        $loadView = new \Core\LoadView('sts/Views/home', null);
+        $loadView->loadView();
+    }
 }
