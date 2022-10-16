6e96c521a0bb09db505bb18564ddd7784d6c598e
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-16 14:13:13 -0300

    Controller login
    Dividi o antigo método index() por outros dois:
        -index;
        -createNewUser.
    Quando realizado o login o usuário é redirecionado para a tela home

--- backend/nossa versão/app/sts/Controllers/Login.php
+++ backend/nossa versão/app/sts/Controllers/Login.php
@@ -6,30 +6,59 @@ class Login{
     
     private array|string|null $date = [];
     private array|string|null $dataForm;
+    private object $stsLogin;
 
-    public function index(){
-        
+
+    
+    /**     function index()
+     * Método chamado pela UrlController;
+     * Primeiro pode receber informações do formulario login, se receber
+     *      chama o método da classe createNewUser;
+     * Responsável por carregar a tela login por meio de um OBJ LoadView.
+     */
+    public function index(): void
+    {
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
         $this->dataForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-        if(!empty($this->dataForm['AddContMsg'])){
-
+        if(!empty($this->dataForm['AddContMsg']))
+        {
             unset($this->dataForm['AddContMsg']);
-            
-            $stsLogin = new \Sts\Models\StsLogin();
-            $result = $stsLogin->login($this->dataForm);
-            if(!empty($result)){
-                extract($result[0]);
-                echo "Id do usuario: {$idusuario}";
-            }else{
-                echo "Usuario não existente";
-            }
-
-
+            $this->createNewUser();
         }
 
         $loadView = new \Core\LoadView('sts/Views/login', null);
         $loadView->loadView();
-        
     }
+
     
+
+    /**     function createNewUser()
+     * Chamado pelo método da classe index;
+     * Responsavel por verificar se o email e senha do cliente existe no
+     *      banco de dados. Faz isso por meio de um OBJ StsLogin;
+     * Se existir uma conta com as informações passadas, o usuario é 
+     *      redirecionado para a tela home, se não permanece na tela login.
+     */
+    private function createNewUser(): void
+    {
+        $this->stsLogin = new \Sts\Models\StsLogin();
+        $result = $this->stsLogin->login($this->dataForm);
+
+        if(!empty($result))
+        {
+            extract($result[0]);
+
+            $_SESSION['idusuario'] = $idusuario;
+            $_SESSION['msg'] = "<p style='color:gree;'> Login realizado com sucesso </p>";
+
+            header("Location: http://localhost/Clinica/Home");
+        }else
+        {
+            $_SESSION['msg'] = "<p style='color:red;'> Usuario não existe </p>";
+        }
+    }
 }
