96bfebf26de8106c2c0a0ac5901a35e74c4dff35
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-22 21:57:12 -0300

    Update servicos.php

--- backend/nossa versão/app/sts/Views/servicos.php
+++ backend/nossa versão/app/sts/Views/servicos.php
@@ -1,3 +1,17 @@
+<?php
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    if(isset($_SESSION['msg'])){
+        echo "Mensagem: " . $_SESSION['msg'] . "<br>";
+        unset($_SESSION['msg']);    
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,18 +23,7 @@
 <body>
 
     <?php
-        include 'app/sts/Controllers/helpers/protect.php';
-
-        if(isset($_SESSION['msg']))
-        {
-            echo "Mensagem: " . $_SESSION['msg'];     
-        }
-        if(isset($_SESSION['idusuario']))
-        {
-            echo $_SESSION['idusuario'] . "<br>";
-        }
-
-        echo "Tela serviços html";
+        echo "Tela servicos html";
         echo "<pre>"; var_dump($this->data); echo "</pre>";
     ?>
     
