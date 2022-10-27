bb50e1a0d51d9c931b10616b3f61c20f8bd47112
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-27 16:19:58 -0300

    Comentando as function

--- backend/nossa versão/app/sts/Models/StsCadastroPet.php
+++ backend/nossa versão/app/sts/Models/StsCadastroPet.php
@@ -5,6 +5,9 @@ namespace Sts\Models;
 class StsCadastroPet
 {
     
+    /**     function dataPet()
+     * Function pública para pegar dados da tabela raca_pet
+     */
     public function dataPet(string $tipoAnimal): array|null
     {
         $stsSelect = new \Sts\Models\helpers\StsSelect();
@@ -17,6 +20,11 @@ class StsCadastroPet
         }
     }
 
+
+    
+    /**     function createPet()
+     * Function pública para criar um novo pet
+     */
     public function createPet(array $data): string|null
     {
         $stsCreate = new \Sts\Models\helpers\StsCreate();
