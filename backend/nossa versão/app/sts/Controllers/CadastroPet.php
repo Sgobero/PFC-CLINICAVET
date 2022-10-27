7468d42b77df023178ce91b6119f508e7ed8e57a
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-27 16:10:55 -0300

    header e comentários
    header usando a constante URL e comentei tbm as funções da controller

--- backend/nossa versão/app/sts/Controllers/CadastroPet.php
+++ backend/nossa versão/app/sts/Controllers/CadastroPet.php
@@ -15,7 +15,11 @@ class CadastroPet
     private string|null $result;
     
 
-
+    /**     function index()
+     * Chamda pela UrlController
+     * Responsavel por carregar e pegar as informações de dois formularios
+     *      diferentes da view sobreCliente
+     */
     public function index(): void
     {
         $this->dataForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
@@ -40,7 +44,8 @@ class CadastroPet
 
             if(!empty($this->result)){
                 $_SESSION['msg'] = "Pet Cadastrado com sucesso";
-                header("Location: http://localhost/Clinica/Home");
+                $header = URL . "Home";
+                header("Location: {$header}");
             }else{
                 $_SESSION['msg'] = "Erro ao cadastrar usuario, entre em contato com o ADM: " . EMAILADM;
             }
@@ -51,6 +56,10 @@ class CadastroPet
 
 
 
+    /**     function getDataPet()
+     * Function para pegar os dados da tabela tipo_pet 
+     *      por meio de um OBJ StsCadastroPet
+     */
     private function getDataPet(): void
     {
         $stsPet = new \Sts\Models\StsCadastroPet();
@@ -59,6 +68,9 @@ class CadastroPet
 
 
 
+    /**     function createNewPet()
+     *Function para criar no pet por meio de um OBJ StsCadastroPet
+     */
     private function createNewPet()
     {
         $stsPet = new \Sts\Models\StsCadastroPet();
@@ -66,7 +78,11 @@ class CadastroPet
     }   
 
 
-    
+
+    /**     function view()
+     * Function para carregar a view cadastroPet 
+     *      por meio de um OBJ LoadView
+     */
     private function view(): void
     {
         $loadView = new \Core\LoadView('sts/Views/cadastroPet', $this->data, $this->whichForm);
@@ -75,3 +91,4 @@ class CadastroPet
 }
 
 ?>
+
