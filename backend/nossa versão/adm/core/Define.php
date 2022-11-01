a7672e076b7128281359e261b98e556f3fef9df1
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-31 23:40:47 -0300

    Update Define.php

--- backend/nossa versão/adm/core/Define.php
+++ backend/nossa versão/adm/core/Define.php
@@ -4,16 +4,10 @@ namespace Core;
 
 abstract class Define
 {
-    /**
-     * Possui as constantes com as configurações.
-     * Configurações de endereço do projeto.
-     * Página principal do projeto.
-     * Credenciais de acesso ao banco de dados
-     * E-mail do administrador.
-     */
-
+    
     protected function configAdm(): void
     {
+        
         define('URL', 'http://localhost/Clinica/');
         define('URLADM', 'http://localhost/Clinica/adm/');
 
@@ -21,12 +15,12 @@ abstract class Define
         define('METODO', 'index');
         define('CONTROLLERERRO', 'Login');
 
+        define('EMAILADM', 'matheus.laurentino.ifpr@gmail.com');
+
         define('HOST', 'localhost');
         define('USER', 'root');
         define('PASS', '');
-        define('DBNAME', 'celke');
-        define('PORT', 3306);
-
-        define('EMAILADM', 'cesar@celke.com.br');
+        define('DBNAME', 'clinica_veterinaria');
+   
     }
 }
