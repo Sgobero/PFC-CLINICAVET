39f9f95bc52fa48f560b477f31048d990d6da61d
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-31 23:30:45 -0300

    Update Home.php

--- backend/nossa versão/app/sts/Controllers/Home.php
+++ backend/nossa versão/app/sts/Controllers/Home.php
@@ -8,17 +8,17 @@ class Home{
     {   
         $dataForm = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
         if(!empty($dataForm['session_destroy']))
         {
             unset($dataForm['session_destroy']);
-            
-            if(!isset($_SESSION)){
-                session_start();
-            }
-            
             session_destroy();
         }
-        $loadView = new \Core\LoadView('sts/Views/home', null);
+        
+        $loadView = new \Core\LoadView('sts/Views/home', null, null);
         $loadView->loadView();
     }
 }
