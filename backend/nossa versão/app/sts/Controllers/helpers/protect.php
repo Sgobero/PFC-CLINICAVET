a6581bfefc437a6b1a540a2241c2b048f5bae4ea
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-10-31 23:26:03 -0300

    Sistema de erro
    caso o usuario não esteja logado ele é redirecionado para a tela de erro case 7

--- backend/nossa versão/app/sts/Controllers/helpers/protect.php
+++ backend/nossa versão/app/sts/Controllers/helpers/protect.php
@@ -1,12 +1,10 @@
 <?php
 
-//namespace Sts\Controllers\helpers;
-
 if(!isset($_SESSION)) {
     session_start();
 }
 
 if(!isset($_SESSION['idusuario'])) {
-    die("Você não pode acessar esta página porque não está logado.");
-    echo "id funcionando";
+    $header = URL . "Erro?case=7"; // Erro 007
+    header("Location: {$header}");
 }
