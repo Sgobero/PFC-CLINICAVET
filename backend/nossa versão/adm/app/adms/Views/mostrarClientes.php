2646d28ce1185334bceebc7fd1c5c5704cf44811
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-11-09 22:40:40 -0300

    Update mostrarClientes.php

--- backend/nossa versão/adm/app/adms/Views/mostrarClientes.php
+++ backend/nossa versão/adm/app/adms/Views/mostrarClientes.php
@@ -1,20 +1,9 @@
 <?php 
 
         extract($this->data);
-       
-       /* for($x = 0; $x < count($this->data); $x++){
-            $clientes = $this->data[$x];
-            extract($clientes);
-            
-            echo   $x+1 . ": " . $idusuario;
-            echo   $x+1 . ": " . $nome_usuario  ;
-            echo   $x+1 . ": " . $tipo_usuario  ;
-            echo   $x+1 . ": " . $email;
-            echo "<br>";
-
-     }*/
 
     echo "LISTA DE USUÁRIOS" . "<br> <br>";
+    
      for($x = 0; $x < count($this->data); $x++)
         {
             $lista = $this->data[$x];
